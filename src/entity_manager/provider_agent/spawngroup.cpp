5fff1da0f81a773a1412701875ea5eca384c8a30
Wend4r2 <47463683+Wend4r@users.noreply.github.com>
2023-11-29 06:42:36 +0300

    Push the creation base of spawn group

---
+++ src/entity_manager/provider_agent/spawngroup.cpp
@@ -0,0 +1,134 @@
+#include "spawngroup.h"
+
+#include <iserver.h>
+#include <mathlib/mathlib.h>
+
+extern INetworkServerService *g_pNetworkServerService;
+
+EntityManager::SpawnGroup::~SpawnGroup()
+{
+	SpawnGroupHandle_t hSpawnGroup = this->m_hSpawnGroup;
+
+	if(hSpawnGroup != INVALID_SPAWN_GROUP)
+	{
+		// engine->UnloadSpawnGroup(hSpawnGroup, kSGUO_None);
+		INetworkGameServer *pNewServer = g_pNetworkServerService->GetIGameServer();
+
+		if(pNewServer)
+		{
+			pNewServer->AsyncUnloadSpawnGroup(hSpawnGroup, kSGUO_None);
+		}
+	}
+}
+
+/*
+EntityManager::SpawnGroup::Status EntityManager::SpawnGroup::GetStatus()
+{
+	SpawnGroupHandle_t hSpawnGroup = this->m_hSpawnGroup;
+
+	if(hSpawnGroup != -1)
+	{
+		if(engine->IsSpawnGroupLoading(hSpawnGroup))
+		{
+			return kEMSGS_InProgress;
+		}
+		else if(engine->IsSpawnGroupLoaded(hSpawnGroup))
+		{
+			return kEMSGS_IsLoaded;
+		}
+		else
+		{
+			return kEMSGS_Broken;
+		}
+	}
+
+	return kEMSGS_None;
+}
+*/
+
+int EntityManager::SpawnGroup::GetStatus()
+{
+	SpawnGroupHandle_t hSpawnGroup = this->m_hSpawnGroup;
+
+	if(hSpawnGroup != INVALID_SPAWN_GROUP)
+	{
+		INetworkGameServer *pNewServer = g_pNetworkServerService->GetIGameServer();
+
+		if(pNewServer)
+		{
+			return pNewServer->GetSpawnGroupLoadingStatus(hSpawnGroup);
+		}
+	}
+
+	return 0;
+}
+
+/*
+bool EntityManager::SpawnGroup::IsResidentOrStreaming(SpawnGroupHandle_t hSpawnGroup)
+{
+	return engine->IsSpawnGroupLoading(hSpawnGroup) || engine->IsSpawnGroupLoaded(hSpawnGroup);
+}
+*/
+
+bool EntityManager::SpawnGroup::IsResidentOrStreaming(SpawnGroupHandle_t hSpawnGroup)
+{
+	INetworkGameServer *pNewServer = g_pNetworkServerService->GetIGameServer();
+
+	if(pNewServer)
+	{
+		return pNewServer->GetSpawnGroupLoadingStatus(hSpawnGroup) > 0;
+	}
+
+	return false;
+}
+
+const Vector &EntityManager::SpawnGroup::GetLandmarkOffset()
+{
+	return this->m_vecLandmarkOffset;
+}
+
+bool EntityManager::SpawnGroup::Start(const char *pLevelName, const char *pSpawnGroupFilterName, float flTimeoutInterval, const char *pLandmarkName, SpawnGroupHandle_t hOwner)
+{
+	INetworkGameServer *pNewServer = g_pNetworkServerService->GetIGameServer();
+
+	if(!pNewServer)
+	{
+		return false;
+	}
+
+	this->m_sLevelName = pLevelName;
+	this->m_sLandmarkName = pLandmarkName;
+
+	if(pLevelName && pLevelName[0])
+	{
+		SpawnGroupHandle_t hSpawnGroup = pNewServer->FindSpawnGroupByName(pLevelName);
+
+		if(IsResidentOrStreaming(hSpawnGroup))
+		{
+			return false;
+		}
+	}
+
+	SpawnGroupDesc_t desc;
+
+	desc.m_hOwner = hOwner;
+	desc.m_pWorldOffsetCallback = this;
+	desc.m_sWorldName = this->m_sLevelName;
+	desc.m_sEntityFilterName = pSpawnGroupFilterName;
+	desc.m_flTimeoutInterval = flTimeoutInterval;
+	desc.m_bCreateClientEntitiesOnLaterConnectingClients = true;
+
+	pNewServer->LoadSpawnGroup(desc);
+
+	return true;
+}
+
+matrix3x4_t EntityManager::SpawnGroup::ComputeWorldOrigin(const char *pWorldName, SpawnGroupHandle_t hSpawnGroup, IWorld *pWorld)
+{
+	matrix3x4_t res;
+
+	res.SetToIdentity();
+	res.SetOrigin(this->GetLandmarkOffset());
+
+	return res;
+}
---
+++ src/entity_manager/provider_agent/spawngroup.h
@@ -0,0 +1,33 @@
+#ifndef _INCLUDE_METAMOD_SOURCE_ENTITY_MANAGER_PROVIDER_AGENT_SPAWNGROUP_
+#define _INCLUDE_METAMOD_SOURCE_ENTITY_MANAGER_PROVIDER_AGENT_SPAWNGROUP_
+
+#include <gamesystems/spawngroup_manager.h>
+#include <worldrenderer/icomputeworldorigin.h>
+
+#define INVALID_SPAWN_GROUP ((SpawnGroupHandle_t)-1)
+
+namespace EntityManager
+{
+	class SpawnGroup : public IComputeWorldOriginCallback
+	{
+	public:
+		~SpawnGroup();
+
+		int GetStatus();
+		static bool IsResidentOrStreaming(SpawnGroupHandle_t hSpawnGroup);
+		const Vector &GetLandmarkOffset();
+
+		bool Start(const char *pLevelName, const char *pSpawnGroupFilterName = NULL, float flTimeoutInterval = 0.0f, const char *pLandmarkName = NULL, SpawnGroupHandle_t hOwner = INVALID_SPAWN_GROUP);
+
+	public: // IComputeWorldOriginCallback
+		matrix3x4_t ComputeWorldOrigin(const char *pWorldName, SpawnGroupHandle_t hSpawnGroup, IWorld *pWorld);
+
+	private:
+		SpawnGroupHandle_t m_hSpawnGroup = INVALID_SPAWN_GROUP;
+		CUtlString m_sLevelName;
+		CUtlString m_sLandmarkName;
+		Vector m_vecLandmarkOffset;
+	};
+};
+
+#endif // _INCLUDE_METAMOD_SOURCE_ENTITY_MANAGER_PROVIDER_AGENT_SPAWNGROUP_
