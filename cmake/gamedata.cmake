40cceb9c968fb7d44790d84d0192544029c5d9af
Wend4r <47463683+Wend4r@users.noreply.github.com>
2024-08-16 23:50:54 +0300

    Update GameData module

--- cmake/gamedata.cmake
+++ cmake/gamedata.cmake
@@ -6,7 +6,7 @@ if(NOT GAMEDATA_DIR)
 	message(FATAL_ERROR "GAMEDATA_DIR is empty")
 endif()
 
-set(GAMEDATA_BINARY_DIR "gamedata")
+set(GAMEDATA_BINARY_DIR "s2u-gamedata")
 set(GAMEDATA_INCLUDE_DIR "${GAMEDATA_DIR}/include")
 
 add_subdirectory(${GAMEDATA_DIR} ${GAMEDATA_BINARY_DIR})
