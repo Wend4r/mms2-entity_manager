b7110470c56db4cde7365f540dd543383cef94f2
Wend4r <47463683+Wend4r@users.noreply.github.com>
2024-08-16 23:29:10 +0300

    Update Any Config module

--- cmake/any_config.cmake
+++ cmake/any_config.cmake
@@ -6,7 +6,7 @@ if(NOT ANY_CONFIG_DIR)
 	message(FATAL_ERROR "ANY_CONFIG_DIR is empty")
 endif()
 
-set(ANY_CONFIG_BINARY_DIR "any_config")
+set(ANY_CONFIG_BINARY_DIR "s2u-any_config")
 set(ANY_CONFIG_INCLUDE_DIR "${ANY_CONFIG_DIR}/include")
 
 add_subdirectory(${ANY_CONFIG_DIR} ${ANY_CONFIG_BINARY_DIR})
