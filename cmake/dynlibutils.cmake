1d718aebc7cadaf1262dc9acad39f5720bdae19f
Wend4r <47463683+Wend4r@users.noreply.github.com>
2024-08-17 01:00:37 +0300

    CMake: resolve a conflict with DynLibUtils module

--- cmake/dynlibutils.cmake
+++ cmake/dynlibutils.cmake
@@ -6,7 +6,9 @@ if(NOT DYNLIBUTILS_DIR)
 	message(FATAL_ERROR "DYNLIBUTILS_DIR is empty")
 endif()
 
-set(DYNLIBUTILS_BINARY_DIR "memory_utils")
+# GameData already have DynLibUtils library!
+
+# set(DYNLIBUTILS_BINARY_DIR "cpp-memory_utils")
 set(DYNLIBUTILS_INCLUDE_DIR "${DYNLIBUTILS_DIR}/include")
 
-# add_subdirectory(${DYNLIBUTILS_DIR} ${DYNLIBUTILS_BINARY_DIR}) # GameData already have DynLibUtils library
+# add_subdirectory(${DYNLIBUTILS_DIR} ${DYNLIBUTILS_BINARY_DIR})
