976f728eaf401c71675a2603faebce28b2529a1e
Wend4r <47463683+Wend4r@users.noreply.github.com>
2024-08-17 00:08:24 +0300

    Update Logger module

--- cmake/logger.cmake
+++ cmake/logger.cmake
@@ -6,7 +6,7 @@ if(NOT LOGGER_DIR)
 	message(FATAL_ERROR "LOGGER_DIR is empty")
 endif()
 
-set(LOGGER_BINARY_DIR "logger")
+set(LOGGER_BINARY_DIR "s2u-logger")
 set(LOGGER_INCLUDE_DIR "${LOGGER_DIR}/include")
 
 add_subdirectory(${LOGGER_DIR} ${LOGGER_BINARY_DIR})
